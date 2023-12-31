fd5734cf45f239f7326b68f5d12aaf13423e37e2
Viglenis08 <viglenis1986@gmail.com>
2023-12-31 12:20:36 -0300

    version de audio

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var ctx = c.getContext("2d");
 var cwidth, cheight;
 var shells = [];
 var pass= [];
-
 var colors = ['#FF5252', '#FF4081', '#E040FB', '#7C4DFF', '#536DFE', '#448AFF', '#40C4FF', '#18FFFF', '#64FFDA', '#69F0AE', '#B2FF59', '#EEFF41', '#FFFF00', '#FFD740', '#FFAB40', '#FF6E40'];
 
 window.onresize = function() { reset(); }
